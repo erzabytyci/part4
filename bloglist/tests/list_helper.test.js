ff92c1e5d2897231784070e78c77f3de0b918fe3
erzabytyci <151072186+erzabytyci@users.noreply.github.com>
2025-05-18 22:37:21 +0300

    4.4 exercise

--- bloglist/tests/list_helper.test.js
+++ bloglist/tests/list_helper.test.js
@@ -1,10 +1,58 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
 
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
+  const listWithMultipleBlogs = [
+    {
+      _id: '1',
+      title: 'First blog',
+      author: 'Author One',
+      url: 'http://example.com/1',
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: '2',
+      title: 'Second blog',
+      author: 'Author Two',
+      url: 'http://example.com/2',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '3',
+      title: 'Third blog',
+      author: 'Author Three',
+      url: 'http://example.com/3',
+      likes: 12,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('when list has multiple blogs, sums all likes', () => {
+    const result = listHelper.totalLikes(listWithMultipleBlogs)
+    assert.strictEqual(result, 24)
+  })
+
+  test('when list is empty, equals 0', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
 })
