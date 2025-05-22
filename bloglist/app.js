1d82fef73e313380e7600144bd30eafb160929b5
erzabytyci <151072186+erzabytyci@users.noreply.github.com>
2025-05-22 23:51:24 +0300

    4.19 exercise

--- bloglist/app.js
+++ bloglist/app.js
@@ -15,9 +15,9 @@ const mongoUrl = process.env.NODE_ENV === 'test'
 mongoose.connect(mongoUrl);
 
 app.use(express.json());
+app.use(middleware.tokenExtractor);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
-app.use(middleware.tokenExtractor);
 
 module.exports = app;
